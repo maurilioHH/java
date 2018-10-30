27a695b9e7728e8c56c38a92b4cdd2a986d0d889
maurilioHH <mauriliostizzo@gmail.com>
2018-10-30 20:12:25 +0100

    -

--- proSpring5/chapter03/constructor-injection/src/main/java/com/apress/prospring5/ch3/DeclareSpringComponents.java
+++ proSpring5/chapter03/constructor-injection/src/main/java/com/apress/prospring5/ch3/DeclareSpringComponents.java
@@ -3,17 +3,18 @@ package com.apress.prospring5.ch3;
 import com.apress.prospring5.ch2.decoupled.MessageProvider;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class DeclareSpringComponents {
-    public static void main(String... args) {
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-        ctx.load("classpath:spring/app-context-annotation.xml");
-        ctx.refresh();
+public class DeclareSpringComponents
+{
+	public static void main(String... args)
+	{
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:spring/app-context-annotation.xml");
+		ctx.refresh();
 
-        MessageProvider messageProvider = ctx.getBean("provider",
-            MessageProvider.class);
+		MessageProvider messageProvider = ctx.getBean("provider", MessageProvider.class);
 
-        System.out.println(messageProvider.getMessage());
-        ctx.close();
+		System.out.println(messageProvider.getMessage());
+		ctx.close();
 
-    }
+	}
 }
