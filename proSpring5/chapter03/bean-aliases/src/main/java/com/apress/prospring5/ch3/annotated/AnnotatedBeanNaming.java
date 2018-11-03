3cad777155f341f068efcc1e93e81c15e601aaf7
maurilioHH <mauriliostizzo@gmail.com>
2018-11-03 19:28:39 +0100

    -

--- proSpring5/chapter03/bean-aliases/src/main/java/com/apress/prospring5/ch3/annotated/AnnotatedBeanNaming.java
+++ proSpring5/chapter03/bean-aliases/src/main/java/com/apress/prospring5/ch3/annotated/AnnotatedBeanNaming.java
@@ -8,18 +8,15 @@ import java.util.Map;
 /**
  * Created by iuliana.cosmina on 2/19/17.
  */
-public class AnnotatedBeanNaming {
-
-	public static void main(String... args) {
+public class AnnotatedBeanNaming
+{
+	public static void main(String... args)
+	{
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
 		ctx.load("classpath:spring/app-context-annotated.xml");
 		ctx.refresh();
-		Map<String,Singer> beans = ctx.getBeansOfType(Singer.class);
-		beans.entrySet().stream().forEach(b ->
-				System.out.println(
-						"id: " + b.getKey() + "\n aliases: "
-								+ Arrays.toString(ctx.getAliases(b.getKey())) + "\n")
-		);
+		Map<String, Singer> beans = ctx.getBeansOfType(Singer.class);
+		beans.entrySet().stream().forEach(b -> System.out.println("id: " + b.getKey() + "\n aliases: " + Arrays.toString(ctx.getAliases(b.getKey())) + "\n"));
 
 		ctx.close();
 	}
