7993267d419cf562b06db1ce5417b2d0c979b76a
maurilioHH <mauriliostizzo@gmail.com>
2018-11-03 19:56:02 +0100

    -

--- proSpring5/chapter03/bean-aliases/src/main/java/com/apress/prospring5/ch3/annotated/AnnotatedBeanNaming.java
+++ proSpring5/chapter03/bean-aliases/src/main/java/com/apress/prospring5/ch3/annotated/AnnotatedBeanNaming.java
@@ -18,6 +18,5 @@ public class AnnotatedBeanNaming
 		Map<String, Singer> beans = ctx.getBeansOfType(Singer.class);
 		beans.entrySet().stream().forEach(b -> System.out.println("id: " + b.getKey() + "\n aliases: " + Arrays.toString(ctx.getAliases(b.getKey())) + "\n"));
 		ctx.close();
-		
 	}
 }
